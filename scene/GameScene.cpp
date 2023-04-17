47175cdc8080985a96488dd5eed5a6e7bed5e8c1
k022g0007@g.neec.ac.jp <k022g0007@g.neec.ac.jp>
2023-04-17 16:58:01 +0900

    01_02変更

--- scene/GameScene.cpp
+++ scene/GameScene.cpp
@@ -1,19 +1,86 @@
 #include "GameScene.h"
 #include "TextureManager.h"
 #include <cassert>
+#include "ImGuiManager.h"
+#include "PrimitiveDrawer.h"
+#include "AxisIndicator.h"
 
 GameScene::GameScene() {}
 
-GameScene::~GameScene() {}
+GameScene::~GameScene() { 
+	delete sprite_;
+	delete model_;
+	delete debugCamera_;
+}
 
 void GameScene::Initialize() {
 
 	dxCommon_ = DirectXCommon::GetInstance();
 	input_ = Input::GetInstance();
 	audio_ = Audio::GetInstance();
+	//画像を読み込む
+	textureHandle_ = TextureManager::Load("AL3kabi.png");
+	//サウンドの読み込み
+	soundDataHandle_ = audio_->LoadWave("fanfare.wav");
+	
+	//スプライトの生成
+	sprite_ = Sprite::Create(textureHandle_, {100, 50});
+	//3Dモデルの生成
+	model_ = Model::Create();
+	//ワールドトランスフォームの初期化
+	worldTransform_.Initialize();
+	//ビュープロジェクションの初期化
+	viewProjection_.Initialize();
+	//音声再生
+	audio_->PlayWave(soundDataHandle_);
+	voiceHandle_ = audio_->PlayWave(soundDataHandle_, true);
+
+	//ライン描画が参照するビュープロジェクションを指定する(アドレス渡し)
+	PrimitiveDrawer::GetInstance()->SetViewProjection(&viewProjection_);
+	
+	//デバッグカメラの生成
+	debugCamera_ = new DebugCamera(1280, 720);
+
+	//軸方向表示を有効にする
+	AxisIndicator::GetInstance()->SetVisible(true);
+	//軸方向表示が参照するビュープロジェクションを指定する(アドレス渡し)
+	AxisIndicator::GetInstance()->SetTargetViewProjection(&debugCamera_->GetViewProjection());
 }
 
-void GameScene::Update() {}
+void GameScene::Update() { 
+
+	//スプライトの今の座標を取得
+	Vector2 positon = sprite_->GetPosition();
+	//座標を{2,1}移動
+	positon.x += 2.0f;
+	positon.y += 1.0f;
+	//移動した座標をスプライトを反映
+	sprite_->SetPosition(positon);
+
+	//スペースキーを押した瞬間
+	if (input_->TriggerKey(DIK_SPACE)) {
+	//音声停止
+		audio_->StopWave(voiceHandle_);
+	}
+
+	//デバッグテキストの表示
+	ImGui::Begin("Debug1");
+	ImGui::Text("Hoshino Kabi %d.%d.%d", 2050, 12, 31);
+	ImGui::End();
+
+	ImGui::Begin("Debug2");
+	//float3入力ボックス
+	ImGui::InputFloat3("InputFloat3", inputFloat3);
+	//float3スライダー
+	ImGui::SliderFloat3("SliderFloat3", inputFloat3, 0.0f, 1.0f);
+	ImGui::End();
+	//デモウィンドウの表示を有効化
+	ImGui::ShowDemoWindow();
+
+	//デバッグカメラの更新
+	debugCamera_->Update();
+
+}
 
 void GameScene::Draw() {
 
@@ -27,6 +94,8 @@ void GameScene::Draw() {
 	/// <summary>
 	/// ここに背景スプライトの描画処理を追加できる
 	/// </summary>
+	//2D
+	sprite_->Draw();
 
 	// スプライト描画後処理
 	Sprite::PostDraw();
@@ -41,6 +110,12 @@ void GameScene::Draw() {
 	/// <summary>
 	/// ここに3Dオブジェクトの描画処理を追加できる
 	/// </summary>
+	
+	//3Dモデル描画
+	model_->Draw(worldTransform_, debugCamera_->GetViewProjection(), textureHandle_);
+
+	// ラインを描画する
+	PrimitiveDrawer::GetInstance()->DrawLine3d({0, 0, 0}, {0, 10, 0}, {1.0f, 0.0f, 0.0f, 1.0f});
 
 	// 3Dオブジェクト描画後処理
 	Model::PostDraw();
